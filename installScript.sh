faa6cda3f2cfbb43153db3a6b9874a5869b3fe72
adrian.pfleiderer <adrian.pfleiderer@student.uni-augsburg.de>
2020-01-11 19:54:00 +0100

    add nessesary tools

--- installScript.sh
+++ installScript.sh
@@ -11,6 +11,7 @@ echo '-----> XNAT v.1.7'
 echo '-----> Xnat-Ubuntu'
 echo '-----> Docker'
 echo '-----> GIT'
+echo '-----> JAVA'
 echo
 echo Please check:
 echo 1. you run this script on Ubuntu v.18 or bigger
@@ -26,18 +27,27 @@ select yn in 'Yes' 'No'; do
 done
 echo install System!
 echo ---------------------------------------
+sleep 5
 echo
 echo
 echo
 echo
 echo Update System
 echo ---------------------------------------
+sleep 5
 apt-get install sudo
 sudo apt-get update
 sudo apt-get upgrade
 echo
+echo Install Java
+echo ---------------------------------------
+sleep 5
+apt-get install default-jdk
+apt-get install nano
+echo
 echo Install Docker
 echo ---------------------------------------
+sleep 5
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -47,10 +57,12 @@ sudo chmod +x /usr/local/bin/docker-compose
 echo
 echo Install GIT
 echo ---------------------------------------
+sleep 5
 sudo apt install git
 echo
 echo Install XNAT
 echo ---------------------------------------
+sleep 5
 cd /
 sudo git clone https://github.com/XxScottxX/Xnat-Ubuntu.git
 cd Xnat-Ubuntu
